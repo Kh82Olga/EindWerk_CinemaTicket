ea96e45dadb8aaf1c5e52260b957612f4659bce7
Kh82Olga <91885710+Kh82Olga@users.noreply.github.com>
2022-08-16 15:42:53 +0200

    last

--- EindWerk_CinemaTicket/Data/ApplicationDbContext.cs
+++ EindWerk_CinemaTicket/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using EindWerk_CinemaTicket.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,29 @@ namespace EindWerk_CinemaTicket.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Genre>().HasData
+                (
+                new Genre { GenreId = 1, GenreName = "Action"},
+                new Genre { GenreId = 2, GenreName = "Comedy"},
+                new Genre { GenreId = 3, GenreName = "Drama"},
+                new Genre { GenreKey = 4, GenreName = "Fantasy"},
+                new Genre { GenreKey = 5, GenreName = "Horror"},
+                new Genre { GenreKey = 6, GenreName = "Thriller"},
+                new Genre { GenreKey = 7, GenreName = "Western"},
+                new Genre { GenreKey = 8, GenreName = "Romantic"},
+                new Genre { GenreKey = 9, GenreName = "Family"},
+                new Genre { GenreKey = 10, GenreName = "Cartoon"}
+                );
+        }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<CinemaSeat> CinemaSeats { get; set; }
+        public DbSet<TicketOrder> TicketOrders { get; set; }
+        public DbSet<Timetable> Timetables { get; set; }
+
     }
 }
