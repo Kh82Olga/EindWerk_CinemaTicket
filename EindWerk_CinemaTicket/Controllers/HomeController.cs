836cf1a778b4664c5376e6b51676ce94370dd8e0
Kh82Olga <91885710+Kh82Olga@users.noreply.github.com>
2022-08-30 15:35:24 +0200

    30/08nm

--- EindWerk_CinemaTicket/Controllers/HomeController.cs
+++ EindWerk_CinemaTicket/Controllers/HomeController.cs
@@ -12,12 +12,7 @@ namespace EindWerk_CinemaTicket.Controllers
 {
     public class HomeController : Controller
     {
-        //private readonly ILogger<HomeController> _logger;
-
-        //public HomeController(ILogger<HomeController> logger)
-        //{
-        //    _logger = logger;
-        //}
+        
         private readonly ApplicationDbContext _context;
 
         public HomeController(ApplicationDbContext context)
