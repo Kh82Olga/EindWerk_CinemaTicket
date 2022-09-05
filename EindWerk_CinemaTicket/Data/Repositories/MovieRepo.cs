c94ec01b5010112f70120b59b7aafa7c2ecbce26
Kh82Olga <91885710+Kh82Olga@users.noreply.github.com>
2022-09-05 13:03:27 +0200

    30

--- EindWerk_CinemaTicket/Data/Repositories/MovieRepo.cs
+++ EindWerk_CinemaTicket/Data/Repositories/MovieRepo.cs
@@ -1,12 +1,26 @@
 ﻿using EindWerk_CinemaTicket.Data.Interfaces;
 using EindWerk_CinemaTicket.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace EindWerk_CinemaTicket.Data.Repositories
 {
     public class MovieRepo : GenericRepo<Movie>, IMovie
     {
+        private readonly AppDbContext _context;
         public MovieRepo(AppDbContext context) : base(context)
         {
+            _context = context;
+        }
+
+        public async Task<Movie> GetMovieByIdAsync(int id)
+        {
+            var movieDetails=await _context.Movies
+                .Include(c=>c.CinemaHall)
+                .Include(g=>g.Genre)
+                .Include(am=>am.ActorMovies).ThenInclude(a => a.Actor)
+                .FirstOrDefaultAsync(n=>n.Id==id);
+            return movieDetails;
         }
     }
 }
