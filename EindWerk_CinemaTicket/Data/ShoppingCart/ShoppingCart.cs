750df9dda3c5d751ef9a827dbc64203785bd2815
Kh82Olga <kh82olga@protonmail.com>
2022-09-07 18:20:19 +0200

    shop

--- EindWerk_CinemaTicket/Data/ShoppingCart/ShoppingCart.cs
+++ EindWerk_CinemaTicket/Data/ShoppingCart/ShoppingCart.cs
@@ -14,10 +14,35 @@ namespace EindWerk_CinemaTicket.Data.ShoppingCart
         }
         public string ShoppingCartId { get; set; }
         public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+        public void AddToCart(Movie movie)
+        {
+            var shoppingCartItem=_context.ShoppingCartItems.FirstOrDefault(n=>n.Movie.Id== movie.Id && n.ShoppingCartId==ShoppingCartId);
+            if (shoppingCartItem == null)
+            {
+                shoppingCartItem = new ShoppingCartItem()
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Movie = movie,
+                    Amount = 1
+                };
+                _context.ShoppingCartItems.Add(shoppingCartItem);
+            }
+            else
+            {
+                shoppingCartItem.Amount++;
+            }
+            _context.SaveChanges();
+        }
         public List<ShoppingCartItem> GetShoppingCartItems()
         {
             return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId)
                 .Include(n => n.Movie).ToList());
+        }
+        public double GetTotal()
+        {
+            var total = _context.ShoppingCartItems.Where(n=>n.ShoppingCartId==ShoppingCartId)
+                .Select(n=>n.Movie.Price * n.Amount).Sum();
+            return total;
         }
     }
 }
