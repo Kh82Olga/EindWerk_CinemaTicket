724866b54ccfe4f32418f4e0d86c44bc1285814f
Kh82Olga <kh82olga@protonmail.com>
2022-09-10 20:48:44 +0200

    signup

--- EindWerk_CinemaTicket/Controllers/AccountController.cs
+++ EindWerk_CinemaTicket/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using EindWerk_CinemaTicket.Data.ViewModels;
 using EindWerk_CinemaTicket.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace EindWerk_CinemaTicket.Controllers
 {
@@ -21,6 +22,36 @@ namespace EindWerk_CinemaTicket.Controllers
         public IActionResult Login()
         {
             var result = new LoginVM();
+            return View(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult>Login(LoginVM loginVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(loginVM);
+            }
+            var user = await _userManager.FindByEmailAsync(loginVM.EmailAddress);
+            if (user != null)
+            {
+                var paswordCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+                if (paswordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password,false,false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Movie");
+                    }   
+                }
+                TempData["Error"] = "Wrong! Please try again.";
+                return View(loginVM);
+            }
+            TempData["Error"] = "Wrong! Please try again.";
+            return View(loginVM);
+        }
+        public IActionResult Register()
+        {
+            var result = new RegisterVM();
             return View(result);
         }
     }
---
+++ EindWerk_CinemaTicket/Data/ViewModels/RegisterVM.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EindWerk_CinemaTicket.Data.ViewModels
+{
+    public class RegisterVM
+    {
+        [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full Name is required")]
+        public string FullName { get; set; }
+        [Display(Name = "Email address")]
+        [Required(ErrorMessage = "Email Address is required")]
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name ="Confirm password")]
+        [Required(ErrorMessage = "Confirm password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage ="Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
